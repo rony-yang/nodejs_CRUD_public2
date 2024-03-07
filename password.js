f5c2801c45078220085a1d47c0888521fa62d522
Seyoung <seyoung5157@gmail.com>
2024-03-07 19:25:32 +0900

    password 업로드

---
+++ password.js
@@ -0,0 +1,14 @@
+const mysql2 = require("mysql2/promise");
+const _pool = mysql2.createPool({
+	host: "localhost",
+	user: "root",
+	password: "1234",
+	port: 3306,
+	dateStrings: "date",
+	connectionLimit: 100,
+	enableKeepAlive: true
+});
+
+module.exports = {
+	pool: _pool
+};
